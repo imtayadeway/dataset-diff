5c6eb81b0b8725882904e6d8d7fffaa6ead46fb3
Tim Wade <tim@majority.co>
2015-04-20 11:49:05 -0400

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject dataset-diff "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "A library designed to diff datasets and provide data on what needs to change in order to sync them."
+  :url "https://github.com/imtayadeway/dataset-diff"
+  :license {:name "GNU General Public License Version 3"
+            :url "https://www.gnu.org/licenses/gpl.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
